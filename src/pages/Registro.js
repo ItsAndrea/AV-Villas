2462a52f79d641f7f472f68800c2f02d66d40ce2
Andrea Fajardo Gutiérrez <85640313+ItsAndrea@users.noreply.github.com>
2021-12-05 11:44:02 -0500

    Add files via upload

---
+++ src/pages/Registro.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Formreg from "../components/Formreg"
+
+class Registro extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <Formreg />
+            </div>
+        )
+    }
+}
+
+export default Registro
